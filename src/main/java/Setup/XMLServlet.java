dc36990b5bb51c1adadc3665e7a6058971942ee6
mariamzp <s215856@dtu.dk>
2022-11-24 10:56:38 +0100

    gg

--- src/main/java/Setup/XMLServlet.java
+++ src/main/java/Setup/XMLServlet.java
@@ -1,5 +1,6 @@
 package Setup;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
